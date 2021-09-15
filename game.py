8611ab13e0d1430641d243d62e91ffb7019a2cef
jacobf18 <j.e.feitelberg@gmail.com>
2021-09-14 23:44:43 -0400

    sketched out part of ChessGame class

---
+++ game.py
@@ -0,0 +1,15 @@
+import chess
+
+class ChessGame():
+    def __init__(self, white, black, maxTurns = 1000):
+        self.board = chess.Board()
+        self.maxTurns = maxTurns
+
+    def turnWhite(self):
+        pass
+
+    def turnBlack(self):
+        pass
+
+    def play(self):
+        pass
